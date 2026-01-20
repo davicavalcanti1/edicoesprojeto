1ef81380b7b188e2bf1ecc086f2916961a7f0844
davicavalcanti <davicavalcantiaraujo1@gmail.com>
2026-01-20 17:13:03 -0300

    Delete ocorrencias-main directory

--- ocorrencias-main/src/components/forms/subtypes/index.ts
+++
@@ -1,6 +0,0 @@
-export { ErroIdentificacaoForm } from "./ErroIdentificacaoForm";
-export { ExtravasamentoForm } from "./ExtravasamentoForm";
-export { RevisaoExameForm } from "./RevisaoExameForm";
-export { QuedasTraumasForm } from "./QuedasTraumasForm";
-export { ExtravasamentoEnfermagemForm } from "./ExtravasamentoEnfermagemForm";
-export { ReacoesAdversasForm } from "./ReacoesAdversasForm";
